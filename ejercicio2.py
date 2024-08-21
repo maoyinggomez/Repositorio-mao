22243884babb8492470dddbdc0b678f6aade01fd
maoyinggomez <mao.gomez@correounivalle.edu.co>
2024-08-21 11:14:26 -0500

    ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,14 +2,14 @@ class Smartwatch:
     def __init__(self,marca,referencia,color):
         self.marca= marca
         self.referencia= referencia
-        self.color
+        self.color= color
 
 
-gt2= Smartwatch("huawei","gt2","negro)
-gt2Ppro= Smartwatch("huawei","gt2Pro","negro)
+gt2= Smartwatch("huawei","gt2","negro")
+gt2Ppro= Smartwatch("huawei","gt2Pro","negro")
 print(type(gt2))
 print(type(gt2Ppro))
 
 
-print(gt2.marca, gt2.referencia. gt2.color)
-print(gt2Ppro.marca, gt2Ppro.referencia, gt2pro.color)
+print(gt2.marca, gt2.referencia, gt2.color)
+print(gt2Ppro.marca, gt2Ppro.referencia, gt2Ppro.color)
